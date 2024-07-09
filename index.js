74859dd740f67dbde851bd3b92127c3b43977f39
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-09 19:09:21 +0200

    Objects

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let name = 'Mosh'; // String Literal
-let age = 30; // Number Literal
-let isApproved = false; // Boolean Literal
-let firstName = undefined;
-let selectedColor = null;
+let person = {
+  name: 'Greg',
+  age: 30
+};
+console.log(person.name)
 
+//  Dot Notation;
+person.name = 'Juli'
+console.log(person.name)
+
+//  Bracket Notation
+person['name'] = 'Dave';
+console.log(person.name)
