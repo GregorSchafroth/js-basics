839406a1ed5d67d7e4250302674fc3c9d43099f2
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-09 19:38:08 +0200

    Types of Functions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 'green';
-console.log(selectedColors);
-console.log(typeof selectedColors);
-console.log(selectedColors.length);
+function greet(name) {
+  console.log('Hello' + name + ' ' + lastName)
+}
 
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2))
