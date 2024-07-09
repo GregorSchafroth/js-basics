7a04488319fa425a99e09099468c818d601dbbca
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-09 19:02:08 +0200

    Variables: q

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let name = 'Mosh';
+console.log('name');
+
+// Cannot be a reserved keywoard
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot cotain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Greg'; 
+let lastName = 'Scha';
