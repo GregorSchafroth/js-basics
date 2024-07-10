dafc7bce501778681b67994d949baaaf58cb56a7
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 15:37:12 +0200

    arrays: finding elements

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// Arrays: adding elements
+// Arrays: finding elements
 
-const numbers = [3, 4];
+const numbers = [1, 2, 3, 1, 4]
 
-// End
-numbers.push(5, 6)
-console.log(numbers);
+console.log(numbers.indexOf(3)) // 2 (at location 2, because of zero indexing)
+console.log(numbers.indexOf(5)) // -1 (this does not exist)
+console.log(numbers.indexOf('3')) // -1 (type matters - there is no 3 string)
+console.log(numbers.indexOf(1)) // 0
+console.log(numbers.lastIndexOf(1)) // 3
 
-// Beginning
-numbers.unshift(1, 2)
-console.log(numbers);
+console.log(numbers.indexOf(1) !== -1) // true, this shows that 1 exists in the array
+console.log(numbers.includes(1)) // does the same as the line just above
 
-// Middle
-numbers.splice(3, 0, 'a', '我')
-console.log(numbers);
+console.log(numbers.indexOf(1, 2)) // 3 (search begins at index 2)
+
