0802b776f77468a725de55c57831084965a9aea1
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 09:17:04 +0200

    even and odd

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-let speed = 130;
-console.log(checkSpeed(speed));
 
-function checkSpeed(speed) {
-  const speedLimit = 120;
-  const kmPerPoint = 5;
-  const points = Math.floor((speed - speedLimit) / kmPerPoint)
-  if (points <= 0) return 'ok';
-  if (points >= 12) return 'License suspended'
-  if (points == 1) return '1 point'
-  return `${points} points`
+showNumbers(15);
+
+function showNumbers(limit) {
+  for (i = 1; i <= limit; i++){
+    console.log(`${i} ${i % 2 == 0 ? 'EVEN' : 'ODD'}`)
+  } 
 }
