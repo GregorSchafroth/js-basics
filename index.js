17709012f444b74196197635003bfa0c48588f67
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 14:37:22 +0200

    object: date

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-// String primitive
-const message = 'This is my first message  ';
-console.log(typeof message)
-console.log(message.length)
-console.log(message[0])
-console.log(message[1])
-console.log(message.includes('my'))
-console.log(message.includes('not'))
-console.log(message.startsWith('This'))
-console.log(message.startsWith('this'))
-console.log(message.indexOf('my'))
-console.log(message.replace('first', 'second')) 
-console.log(message) //note that the line above does not modify original string
-console.log(message.toUpperCase) 
-console.log(message.toLowerCase) 
-console.log(message.trim) 
-console.log(message.replace('first', 'second\n')) 
+// Date Object
 
-console.log('---')
+const now = new Date()
+console.log(now)
 
-// String objective
-const another = new String('hi')
-console.log(typeof another)
+now.setFullYear('2020')
+console.log(now)
 
+const juliBirthday = new Date('July 10 1993 01:00')
+console.log(juliBirthday)
