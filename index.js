2581122ccc8dcb3eb865fd30b52fde4034536bf0
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 15:18:48 +0200

    object exercise: blog post

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// object exercise: factory and constructor functions
+// object exercise: blog post
 
-// address = {
-//   street: 'Geissfluwheg 27',
-//   city: 'Olten',
-//   zipCode: 4600,
-// }
-
-// factory function
-function createAddress(street, city, zipCode) {
-  return {
-    street: street,
-    city: city,
-    zipCode: zipCode
-  };
+let blogPost = {
+  title: 'post',
+  body: 'body text',
+  author: 'redsheep',
+  views: 123,
+  comments: [
+    {
+      author: 'greensheep',
+      body: 'comment1'
+    },
+    {
+      author: 'bluesheep',
+      body: 'comment2'
+    }
+  ],
+  isLive: false
 }
-const address1 = createAddress('Schweizwerweg 13', 'Olten', 4600);
-console.log(address1);
 
-// constructor function
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-const address2 = new Address('Schweizwerweg 13', 'Olten', 4600);
-console.log(address2);
+console.log(blogPost)
