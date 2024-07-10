6611ea335ea1b441a1edc39d8f78e8bc49c15758
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 07:43:10 +0200

    for...of

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const person = {
-  name: 'Greg',
-  age: 30
-};
-
-for (let key in person)
-  console.log(key, person[key]);
-
 const colors = ['red', 'green', 'blue'];
 
+// for in
 for (let index in colors)
   console.log(index, colors[index])
 
+// for of
+for (let color of colors)
+  console.log(color);
