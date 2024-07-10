0fca7c2a351a48f47637f53bda1de499382f56f9
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 09:46:34 +0200

    stars

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const marks = [40, 60, 95, 70]
+showStars(10);
 
-function calculateGBrade(marks) {
-  let total = 0;
-  for (let mark of marks){
-    total += mark
+function showStars(rows){
+  let stars = ''
+  for (i = 0; i < rows; i++){
+    stars += '*'
+    console.log(stars)
   }
-  const averageMark = total / marks.length;
-  if (averageMark < 60) return 'F'
-  if (averageMark < 70) return 'D'
-  if (averageMark < 80) return 'C'
-  if (averageMark < 90) return 'B'
-  return 'A'
 }
-
-console.log(calculateGBrade(marks));
