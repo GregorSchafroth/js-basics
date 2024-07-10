4e9532f193f78896dbbb622c2b18b5f51f71982b
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 09:43:29 +0200

    calculate grade'

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const movie = {
-  title: 'a',
-  releaseYear: 2018,
-  rating: 4.5,
-  director: 'b'
-};
+const marks = [40, 60, 95, 70]
 
-showProperties(movie);
-
-function showProperties(obj){
-  for (property in obj){
-    console.log(`${property}: ${obj[property]}`)
+function calculateGBrade(marks) {
+  let total = 0;
+  for (let mark of marks){
+    total += mark
   }
+  const averageMark = total / marks.length;
+  if (averageMark < 60) return 'F'
+  if (averageMark < 70) return 'D'
+  if (averageMark < 80) return 'C'
+  if (averageMark < 90) return 'B'
+  return 'A'
 }
+
+console.log(calculateGBrade(marks));
