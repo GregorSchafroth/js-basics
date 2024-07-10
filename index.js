6c43988c67d8b544518371b60073cf5d52fc447f
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 17:54:33 +0200

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 // Arrays: finding elements
 
-const numbers = [1, 2, 3, 1, 4]
+const courses = [
+  { id: 1, name: 'a'},
+  { id: 2, name: 'b'},
+];
 
-console.log(numbers.indexOf(3)) // 2 (at location 2, because of zero indexing)
-console.log(numbers.indexOf(5)) // -1 (this does not exist)
-console.log(numbers.indexOf('3')) // -1 (type matters - there is no 3 string)
-console.log(numbers.indexOf(1)) // 0
-console.log(numbers.lastIndexOf(1)) // 3
-
-console.log(numbers.indexOf(1) !== -1) // true, this shows that 1 exists in the array
-console.log(numbers.includes(1)) // does the same as the line just above
-
-console.log(numbers.indexOf(1, 2)) // 3 (search begins at index 2)
-
+courses.find()
