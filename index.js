78f4cfece411d2d9cdfa53e1af8269cd7ffca986
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-10 15:09:54 +0200

    exercise: factory and constructor functions

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-// object exercise: address
+// object exercise: factory and constructor functions
 
-address = {
-  street: 'Geissfluwheg 27',
-  city: 'Olten',
-  zipCode: 4600,
-}
+// address = {
+//   street: 'Geissfluwheg 27',
+//   city: 'Olten',
+//   zipCode: 4600,
+// }
 
-for (let key in address) {
-  console.log(`${key}: ${address[key]}`);
+// factory function
+function createAddress(street, city, zipCode) {
+  return {
+    street: street,
+    city: city,
+    zipCode: zipCode
+  };
 }
+const address1 = createAddress('Schweizwerweg 13', 'Olten', 4600);
+console.log(address1);
+
+// constructor function
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+const address2 = new Address('Schweizwerweg 13', 'Olten', 4600);
+console.log(address2);
